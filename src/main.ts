54d0de846ca46a967c2e1fe75efd605c59f71c24
xyon3 <reyespauldavid@gmail.com>
2025-05-13 21:25:51 +0800

    cors to all

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ["https://elira-frontend.vercel.app", "http://localhost:3000"],
+        origin: "*",
         methods: "*",
     });
 
